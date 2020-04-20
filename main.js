3f4c8ba9a11c6749d453d2b5c50b9682a812100a
JumarySanchez <sanchezjumary@gmail.com>
2020-04-20 13:27:44 -0400

    :(

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 
 
+const makeRunOn= function(str){
+  return str.split('.').join(',')} // help me with this. I cant figure out how to not remove the last period if the string.
+// maybe an if
 
+
+const stringOut = function(str){
+  return str.split(' ').join('').split('').join(' ')
+  
+}
+
+const nightOwls = function(arr){
+  const notAwake = []
+    for (const person of arr){
+      if (person.localTime>=100 && person.localTime<=400 && !person.asleep){
+      notAwake.push(person)
+    }
+  }
+  return notAwake
+}
+
+const totalScore = function(addUp){
+  let total = 0
+  for (const num of addUp){
+    if (num.multiplier === 1){
+      total += num.score
+    }
+    else if (num.multiplier === undefined){
+      return total *= num.totalScore
+    }
+  }
+  return total
+}
+
+
+const getToBed = function(arr){
+  
+  const everyoneWakeUp = [...arr]
+    for (const person of arr){
+      if (person.localTime>=100 && person.localTime<=400 && !person.asleep){
+      everyoneWakeUp.asleep = true
+    }
+  }
+  return everyoneWakeUp
+}
 
 
 
